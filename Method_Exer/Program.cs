ac4b49da3e4db936dbbc7856818d1caccfafb6b3
unknown <cannicam@gmail.com>
2020-10-26 15:08:22 -0500

    new program

---
+++ Method_Exer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Method_Exer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There...");
+            Console.WriteLine("Who are you?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Well Hello {name}");
+            Console.WriteLine("Let's play a Game...");
+
+
+
+
+
+        }
+    }
+}
